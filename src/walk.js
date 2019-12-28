841871d3fe103dd4b2267e9b999d505b06f21972
Ekene Izukanne <ekeneizukanne@gmail.com>
2019-12-29 00:42:50 +0100

    asynchronous directory traversal

---
+++ src/walk.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var path = require("path");
+
+module.exports = function (path, walkFn) {
+    fs.lstat(path, (err, stats) => {
+        if (err) {
+            walkFn(err, path, stats);
+        }
+
+        traverse(path, stats, walkFn);
+    });
+};
+
+function traverse (root, stats, walkFn) {
+    if (stats.isFile()) {
+        return walkFn(null, root, stats);
+    }
+
+    fs.readdir(root, (err, files) => {
+        walkFn(err, root, stats);
+        for (var i = 0; i < files.length; i++) {
+            var file = path.join(root, files[i]);
+            var info = fs.lstatSync(file);
+            traverse(file, info, walkFn);
+        }
+    });
+}
