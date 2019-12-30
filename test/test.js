6236acc543ca7a272baab770dcc412499a60ee0a
Ekene Izukanne <ekeneizukanne@gmail.com>
2019-12-30 20:07:52 +0100

    test synchronous api for dirs returned

--- test/test.js
+++ test/test.js
@@ -2,6 +2,17 @@ const assert = require("assert");
 const path = require("path");
 const dirwalk = require("../");
 
+var expectedfiles = [
+    path.join(__dirname, "testdir"),
+    path.join(__dirname, "testdir", ".file"),
+    path.join(__dirname, "testdir", "dir1"),
+    path.join(__dirname, "testdir", "dir1", ".dir1file"),
+    path.join(__dirname, "testdir", "dir2"),
+    path.join(__dirname, "testdir", "dir2", ".dir2file"),
+    path.join(__dirname, "testdir", "dir3"),
+    path.join(__dirname, "testdir", "dir3", ".dir3file")
+];
+
 describe("dirwalk", function () {
     describe("walk", function () {
         it("should traverse directory asynchronusly", function (done) {
@@ -23,6 +34,18 @@ describe("dirwalk", function () {
             var files = dirwalk.walkSync(path.join(__dirname, "testdir"));
             foundfiles = files;
             assert.notStrictEqual(foundfiles, undefined);
+            done();
+        });
+
+        it("should return an array of files", function (done) {
+            var files = dirwalk.walkSync(path.join(__dirname, "testdir"));
+            assert.deepStrictEqual(files, expectedfiles);
+            done();
+        });
+
+        it("should find all the files in a directory", function (done) {
+            var files = dirwalk.walkSync(path.join(__dirname, "testdir"));
+            assert.deepStrictEqual(files.length, expectedfiles.length);
             done();
         });
     });
